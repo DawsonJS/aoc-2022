b7bdbf4b1deefd2c4ba8c56d8fe7c9fb133dfa58
Dawson Schraiber <dschraiber@vt.edu>
2022-12-02 10:12:50 -0500

    Day two completed

---
+++ src/day_two.rs
@@ -0,0 +1,108 @@
+#[derive(Debug)]
+#[derive(PartialEq, Eq)]
+#[derive(Copy, Clone)]
+pub enum HandShape {
+    Rock,
+    Paper,
+    Scissors,
+    Invalid
+}
+
+pub enum PuzzlePart {
+    One,
+    Two,
+}
+
+impl HandShape {
+    pub fn win_against(&self) -> HandShape {
+        match self {
+            HandShape::Rock => HandShape::Paper,
+            HandShape::Paper => HandShape::Scissors,
+            HandShape::Scissors => HandShape::Rock,
+            _ => HandShape::Invalid,
+        }
+    }
+
+    pub fn lose_against(&self) -> HandShape {
+        match self {
+            HandShape::Rock => HandShape::Scissors,
+            HandShape::Paper => HandShape::Rock,
+            HandShape::Scissors => HandShape::Paper,
+            _ => HandShape::Invalid,
+        }
+    }
+
+    pub fn decode_hand_shape(&self, code: &str) -> HandShape {
+        match code {
+            "X" => self.lose_against(),
+            "Y" => *self,
+            "Z" => self.win_against(),
+            _ => HandShape::Invalid,
+        }
+    }
+}
+
+pub fn get_hand_shape(code: &str) -> HandShape {
+    match code {
+        "A" => HandShape::Rock,
+        "B" => HandShape::Paper,
+        "C" => HandShape::Scissors,
+        "X" => HandShape::Rock,
+        "Y" => HandShape::Paper,
+        "Z" => HandShape::Scissors,
+        _ => HandShape::Invalid,
+    }
+}
+
+pub fn calc_points(code_line: &str, part: PuzzlePart) -> i32 {
+    let opponent = get_hand_shape(&code_line[..1]);
+    let player = match part {
+            PuzzlePart::One => get_hand_shape(&code_line[2..]),
+            PuzzlePart::Two => opponent.decode_hand_shape(&code_line[2..]),
+    };
+    let mut points: i32 = 0;
+
+    match player {
+        HandShape::Rock => {points += 1;},
+        HandShape::Paper => {points += 2},
+        HandShape::Scissors => {points += 3},
+        _ => {},
+    }
+
+    if player == opponent.win_against() {
+        points += 6;
+    } else if player == opponent {
+        points += 3;
+    }
+
+    points
+}
+
+#[cfg(test)]
+mod day_two_tests {
+    use crate::day_two::*;
+
+    #[test]
+    fn get_hand_shape_test() {
+        let example = "A Y\nB X\nC Z\n";
+        let test = get_hand_shape(&example[..1]);
+        assert_eq!(test, HandShape::Rock);
+    }
+
+    #[test]
+    fn against_test() {
+        let example = HandShape::Rock;
+        assert_eq!(example.win_against(), HandShape::Paper);
+        assert_eq!(example.lose_against(), HandShape::Scissors);
+    }
+
+    #[test]
+    fn calc_points_test() {
+        let example = "A Y\nB X\nC Z\n";
+        let mut test = calc_points(&example[..3], PuzzlePart::One);
+
+        assert_eq!(test, 8);
+        test = calc_points(&example[..3], PuzzlePart::Two);
+        assert_eq!(test, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 pub mod day_one;
+pub mod day_two;
 
 use std::env;
 use std::fs;
 use day_one::*;
+use day_two::*;
 
 
 fn main() {
@@ -30,9 +32,34 @@ fn main() {
             println!("Elf number {} has the greatest number of calories: {} calories", greatest_cal as i32 + 1, calories[greatest_cal]);
             println!("The top three calorie carrying elves are holding a total of {} calories", top_three_elves);
         },
+        2 => {
+            let file_path = &args[2];
+
+            let contents = fs::read_to_string(file_path)
+                .expect("Should have been able to read the file");
+            let mut puzzle_one_scores: Vec<i32> = Vec::new();
+
+            for l in contents.lines() {
+                puzzle_one_scores.push(calc_points(l, PuzzlePart::One));
+            }
+
+            let mut total: i32 = puzzle_one_scores.iter().sum();
+            println!("Total score following assumed code: {}", total);
+
+            let mut puzzle_two_scores: Vec<i32> = Vec::new();
+
+            for l in contents.lines() {
+                puzzle_two_scores.push(calc_points(l, PuzzlePart::Two));
+            }
+
+            total = puzzle_two_scores.iter().sum();
+
+            println!("Total score following correct code: {}", total);
+
+        },
         _ => {
             println!("This day has not been completed yet");
-        }
+        },
     }
 }
 
